d12c5c133d8d1d357459a9c428881e4740afc7a5
Francisco Ponce <franciscojavier.poncerosales@gmail.com>
2024-08-03 19:34:27 +0200

    add: calendar icon

--- src/document/sections/DayPage.jsx
+++ src/document/sections/DayPage.jsx
@@ -1,14 +1,7 @@
-import { Image, Link, Text } from "@react-pdf/renderer";
+import { Link, Text } from "@react-pdf/renderer";
 import { Page } from "./Page";
 import { NAVIGATION_LINK_ID } from "./CalendarNavigate";
-
-const navigationHomeIconSvg = `
-data:image/svg+xml;utf8,
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px">
-  <path d="M0 0h24v24H0z" fill="none"/>
-  <path d="M12 2L2 21h20z"/>
-</svg>
-`;
+import { Calendar } from "../icons/Calendar";
 
 export const DayPage = ({
   date,
@@ -24,18 +17,12 @@ export const DayPage = ({
     <Link
       style={{
         position: 'absolute',
-        top: 0,
-        right: 0,
+        top: 2,
+        right: 2,
       }}
       src={`#${NAVIGATION_LINK_ID}`}
     >
-      <Image
-        src={navigationHomeIconSvg}
-        style={{
-          width: 20,
-          height: 20,
-        }}
-      />
+      <Calendar />
     </Link>
     <Text>Fecha: {getDate()}</Text>
   </Page>
